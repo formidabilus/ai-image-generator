54763fa84206128de8fb7f1e208ae2a567490bfa
formidabilus <chiriac_razvan@ymail.com>
2023-04-04 17:40:23 +0300

    added azure folder, created function app and created azure function getChatGPTSuggestion

---
+++ azure/src/functions/getChatGPTSuggestion.ts
@@ -0,0 +1,23 @@
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+
+export async function getChatGPTSuggestion(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  const name = request.query.get("name") || (await request.text()) || "world";
+
+  return { body: `Hello, ${name}!` };
+}
+
+app.http("getChatGPTSuggestion", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: getChatGPTSuggestion,
+});
